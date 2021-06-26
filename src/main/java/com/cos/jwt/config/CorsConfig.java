ad49031455ec1e6148fe66cf356a681d692bb0b3
sanseongKo <tkstjd56@naver.com>
2021-06-27 02:00:39 +0900

    jwt practice 3

---
+++ src/main/java/com/cos/jwt/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.cos.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+public class CorsConfig {
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);//내서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는것, false면 자바 스크립트가 요청하면 오지않음
+		config.addAllowedOrigin("*");//모든 ip에 응답을 허용
+		config.addAllowedHeader("*");//모든 header에 응답을 허용
+		config.addAllowedMethod("*");//모든 post,get,delete,patch 요청을 허용하겠다.
+		source.registerCorsConfiguration("/api/**", config);
+		return new CorsFilter(source);
+	}
+}
---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.cos.jwt.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private final CorsFilter corsFilter;
+	//jwt기본 고정
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션을 사용하지 않겠다.
+		.and()
+		.addFilter(corsFilter)//@CrossOrigin(인증이 없을 때 문제), 시큐리티 필터에 등록 인증해줘야함
+		.formLogin().disable()//form로그인방식 안씀
+		.httpBasic().disable()//http로그인방식 안씀
+		.authorizeRequests()
+		.antMatchers("/api/v1/user/**")
+		.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+		.antMatchers("/api/v1/manager/**")
+		.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+		.antMatchers("/api/v1/admin/**")
+		.access("hasRole('ROLE_ADMIN')")
+		.anyRequest().permitAll();
+
+		
+	}
+}
