928eafae39618098f1c6ea0be172912879bf8bd0
sanseongKo <tkstjd56@naver.com>
2021-06-27 17:55:24 +0900

    jwt filter practice

---
+++ src/main/java/com/cos/jwt/config/FilterConfig.java
@@ -0,0 +1,27 @@
+package com.cos.jwt.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter2;
+
+@Configuration
+public class FilterConfig {
+	@Bean
+	public FilterRegistrationBean<MyFilter1> filter1(){
+		FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+		bean.addUrlPatterns("/*");
+		bean.setOrder(0);//낮은번호가 필터중 가장먼저 실행됨
+		return bean;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<MyFilter2> filter2(){
+		FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
+		bean.addUrlPatterns("/*");
+		bean.setOrder(1);//낮은번호가 필터중 가장먼저 실행됨
+		return bean;
+	}//리퀘스트 요청이 오면 실행이 됨 필터들은
+}
--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -6,7 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
+
+import com.cos.jwt.filter.MyFilter3;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	//jwt기본 고정
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+		http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션을 사용하지 않겠다.
 		.and()
